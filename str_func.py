6be228023dbe68b1e8616611fc6e3c8fe2272596
Taulan Eraliev <etaulan@mail.ru>
2024-06-08 14:48:10 +0300

    docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,5 @@
-s = 'qwertyuiopasdfghjklzxcvbnm'
-print(s.upper())
+def capitalize_first_letter(s):
+    for i, char in enumerate(s):
+        if char.isalpha():
+            return s[:i] + char.upper() + s[i+1:]
+    return s
